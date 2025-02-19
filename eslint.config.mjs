e3658dd53bd83a1fd31891150b819a852f493066
devenkumar1 <devenkumar540@gmail.com>
2025-02-19 20:02:24 +0530

    fixed eslint warning

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+
+  // Add custom rules
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": "off", // Disable the no-unused-vars rule
+    },
+  },
 ];
 
 export default eslintConfig;
