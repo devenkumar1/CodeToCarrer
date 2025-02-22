5f217374224dd93f463467f63a29e822c9f1e551
devenkumar1 <devenkumar540@gmail.com>
2025-02-22 21:19:02 +0530

    merged pull request

--- components/BasicEditor.tsx
+++ components/BasicEditor.tsx
@@ -5,11 +5,11 @@ import { highlight, languages } from 'prismjs';
 
 export const BasicEditor = ({ value, onChange, language }: { value: string, onChange: (value: string) => void, language: string }) => {
   return (
-    <div className="md:h-[80vh] h-[50vh]  overflow-auto border border-solid border-[#ddd] rounded-lg " >
+    <div className="md:h-[80vh] h-[50vh] overflow-auto border border-solid border-[#ddd] rounded-lg " >
       <Editor
         value={value}
         onValueChange={onChange}
-        highlight={(code) => highlight(code, languages[language] || languages.js, language)} 
+        highlight={(code) => highlight(code, languages[language] || languages.js, language)}
         padding={10}
         className="min-h-[477px] text-black dark:text-white dark:bg-[#303030] bg-[#f5f5f5] text-size-[16px]"
       />
